6b2c9ddb3c14d593a729ce51eb6ba4c7c605e94c
Cam Tangalakis-Lippert <cami.lippert@gmail.com>
2021-11-04 09:45:10 -0700

    tested user auth

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,35 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { User } = require('../../db/models');
+const { restoreUser, requireAuth, setTokenCookie } = require('../../utils/auth.js');
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-});
+
+
+
+
+//-------------test routes -----------------
+// router.get('/require-auth', requireAuth, (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// router.get('/restore-user', restoreUser, (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       },
+//     })
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
+
+// router.post('/test', function(req, res) {
+//     res.json({ requestBody: req.body });
+// });
 
 module.exports = router;
