f90d489a4930e12b054e59a10675825e4fe82e5b
Cam Tangalakis-Lippert <cami.lippert@gmail.com>
2021-11-04 08:52:23 -0700

    error handlers

--- backend/app.js
+++ backend/app.js
@@ -9,12 +9,14 @@ const { environment } = require('./config');
 const isProduction = environment === 'production';
 const app = express()
 const routes = require('./routes')
+const { ValidationError } = require('sequelize');
 
 app.use(morgan('dev'))
 app.use(cookieParser())
 app.use(express.json())
 if (!isProduction) app.use(cors());
 app.use(helmet({contentSecurityPolicy: false}));
+
 app.use(csurf({cookie: {
     secure: isProduction,
     sameSite: isProduction && "Lax",
@@ -23,5 +25,31 @@ app.use(csurf({cookie: {
 
 app.use(routes)
 
+app.use(function (_req, _res, next) {
+    const err = new Error('The requested resource could not be found')
+    err.title = "Resource Not Found"
+    err.errors = ["The requested resource could not be found"]
+    err.status = 404
+    next(err)
+})
+
+app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+      err.errors = err.errors.map((e) => e.message);
+      err.title = 'Validation error';
+    }
+    next(err);
+});
+
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack,
+    });
+  });
 
 module.exports = app
